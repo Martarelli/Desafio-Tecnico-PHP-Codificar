9aa0c9d3dc68ad96edd4150be620b0a39518d9eb
Martarelli <renanmartarelli@gmail.com>
2023-07-07 13:28:40 -0300

    filtro dos orcamentos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,22 +2,45 @@
 
 @section('content')
 <a href="{{ url('/create') }}" class="btn btn-primary m-3">Criar Orçamento</a>
-<div class="w-100 d-flex justify-content-center">
+<div class="w-100 d-flex flex-column justify-content-center">
+    <div class="container">
+        <form class="d-flex" action="/" method="get">
+            <div class="form-group">
+                <label class="mt-3" for="cliente">Cliente</label>
+                <input class="w-100" type="text" name="cliente" value={{!empty($filtro) ? $filtro['cliente'] : ''}}>
+            </div>
+            <div class="form-group">
+                <label class="mt-3" for="vendedor">Vendedor</label>
+                <input class="w-100" type="text" name="vendedor" value={{!empty($filtro) ? $filtro['vendedor'] : ''}}>
+            </div>
+            <div class="form-group">
+                <label class="mt-3" for="dataInicial">Data Inicial</label>
+                <input class="w-100" type="date" name="dataInicial" value={{!empty($filtro) ? $filtro['dataInicial'] : ''}}>
+            </div>
+            <div class="form-group">
+                <label class="mt-3" for="dataFinal">Data Final</label>
+                <input class="w-100" type="date" name="dataFinal" value={{!empty($filtro) ? $filtro['dataFinal'] : ''}}>
+            </div>
+            <button type="submit" class="btn btn-primary w-100 mt-4">Filtrar</button>
+            <a href="/" class="btn btn-secondary w-100 mt-4">Limpar Filtro</a>
+        </form>
+
+    </div>
 
     <table class="table table-striped table-bordered table-hover table-light m-3">
         <thead>
-          <tr>
-            <th class="text-center" scope="col">Cliente</th>
-            <th class="text-center" scope="col">Vendedor</th>
-            <th class="text-center" scope="col">Descrição</th>
-            <th class="text-center" scope="col">Valor</th>
-            <th class="text-center" scope="col">Criação</th>
-            <th class="text-center" scope="col">Ultima Modificação</th>
-            <th class="text-center" scope="col">Ação</th>
-          </tr>
+            <tr>
+                <th class="text-center" scope="col">Cliente</th>
+                <th class="text-center" scope="col">Vendedor</th>
+                <th class="text-center" scope="col">Descrição</th>
+                <th class="text-center" scope="col">Valor</th>
+                <th class="text-center" scope="col">Criação</th>
+                <th class="text-center" scope="col">Ultima Modificação</th>
+                <th class="text-center" scope="col">Ação</th>
+            </tr>
         </thead>
         <tbody>
-            @foreach ($orcamentos as $orcam)
+        @foreach ($orcamentos as $orcam)
             <tr>
                 <td class="text-center" style="width: 15%;">{{$orcam->cliente}}</td>
                 <td class="text-center" style="width: 15%;">{{$orcam->vendedor}}</td>
@@ -30,8 +53,8 @@
                     <a href="/destroy/{{$orcam->id}}" class="btn btn-danger">Excluir</a>
                 </td>
             </tr>
-            @endforeach
+        @endforeach
         </tbody>
-      </table>
+    </table>
 </div>
 @endsection
